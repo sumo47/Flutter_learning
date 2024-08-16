f3e1e4d74916e4c7d15623a3b4c86fa658680362
Sumit <sumokhan76342@gmail.com>
2024-08-16 20:13:53 +0530

    first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,12 @@
 import 'package:flutter/material.dart';
 
+// The main function is the starting point of all flutter apps
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter app"),
-          backgroundColor: Color.fromARGB(255, 238, 224, 103),
-        ),
-        body: Center(
-          child: Row(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                // color: Colors.amber.shade600, // we have to use single style property
-                decoration: BoxDecoration (
-                  color:Colors.red,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Text(
-                    "world",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ),
-              SizedBox(width: 20,),
-              Container(
-                child: Center(
-                  child: Text(
-                    "Hello",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ) ,
-                width: 200,
-                height: 200,
-                color: Colors.greenAccent.shade400,
-              )
-
-            ],
-          ),
-        ),
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
       ),
-    );
-  }
+    ),
+  );
 }
