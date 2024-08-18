ae5d9f66438fda0384ef069eb3cc1080ba9dfc6d
Sumit <sumokhan76342@gmail.com>
2024-08-18 20:13:29 +0530

    challage for i am poor

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 
-// The main function is the starting point of all flutter apps
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.yellow.shade500,
         appBar: AppBar(
-          title: Text("I am Rich"),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text("I am poor"),
+          backgroundColor: Colors.yellow.shade800,
         ),
         body: Center(
-          child: Image(
-            image:AssetImage('images/diamond.png')
-          ),
-        ),
+            child: Image(
+          image: AssetImage("images/diamond.png"),
+        )),
       ),
     ),
   );
 }
+
+// The main function is the starting point of all flutter apps
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.blueGrey,
+//         appBar: AppBar(
+//           title: Text("I am Rich"),
+//           backgroundColor: Colors.blueGrey[900],
+//         ),
+//         body: Center(
+//           child: Image(
+//             image:AssetImage('images/diamond.png')
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
