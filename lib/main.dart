27f5d69ccae97cea5b12dbf54c4d4bc5e9387846
Sumit <sumokhan76342@gmail.com>
2024-08-19 09:58:51 +0530

    marign and padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.yellow.shade500,
-        appBar: AppBar(
-          title: Text("I am poor"),
-          backgroundColor: Colors.yellow.shade800,
-        ),
-        body: Center(
-            child: Image(
-          image: AssetImage("images/diamond.png"),
-        )),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
-// The main function is the starting point of all flutter apps
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.blueGrey,
-//         appBar: AppBar(
-//           title: Text("I am Rich"),
-//           backgroundColor: Colors.blueGrey[900],
-//         ),
-//         body: Center(
-//           child: Image(
-//             image:AssetImage('images/diamond.png')
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            // margin: EdgeInsets.all(50),
+            // margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            // margin: EdgeInsets.only(top: 20),
+            // margin: EdgeInsets.zero,
+            margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
+
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            width: 100,
+            height: 100,
+            child: Text("Hello",style: TextStyle(color: Colors.black),),
+          
+          ),
+        ),
+      ),
+    );
+  }
+}
