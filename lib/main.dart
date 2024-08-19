49286acea347db7d0e809e6045570dfa81f411aa
Sumit <sumokhan76342@gmail.com>
2024-08-19 12:05:14 +0530

    font added

--- lib/main.dart
+++ lib/main.dart
@@ -13,56 +13,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            // mainAxisSize: MainAxisSize.min, // cover only minimum size
-            // verticalDirection: VerticalDirection.up,
+          child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.end,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.red,
-                width: 100,
-                // height: 100,
-                child: Text(
-                  "container 1",
-                  style: TextStyle(color: Colors.black),
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/sumit.jpg'),
+              ),
+              Text(
+                "Sumit Ku",
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: "Pacifico"),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source Code Pro",
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
                 ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green[500],
-                  ),
-                ],
-              ),
-
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.blue,
-                // width: double.infinity,
-                // height: 100,
-                child: Text(
-                  "container 2",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              //   Container(
-              //     width: double.infinity,
-              //   ),
             ],
           ),
         ),
