82a4a35e308b621d82123513fd300f9dc989e09e
Sumit <sumokhan76342@gmail.com>
2024-08-20 12:58:43 +0530

    Expeded, Padding ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,76 +12,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/sumit.jpg'),
-              ),
-              Text(
-                "Sumit Ku",
-                style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontFamily: "Pacifico"),
-              ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontFamily: "Source Code Pro",
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontSize: 20.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(color: Colors.teal[100],),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    "+91 7481917452",
-                    style: TextStyle(
-                      color: Colors.teal[900],
-                      fontSize: 20.0,
-                      fontFamily: "Source Sans 3",
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "thesumitsah47@gmail.com",
-                      style: TextStyle(
-                        color: Colors.teal[900],
-                        fontSize: 20.0,
-                        fontFamily: "Source Sans 3",
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  )),
-            ],
-          ),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+        ],
       ),
     );
   }
