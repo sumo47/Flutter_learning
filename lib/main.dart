942d873964ddb661a0d91e3a987dd96d1ebed015
Sumit <sumokhan76342@gmail.com>
2024-08-22 19:54:23 +0530

    audioplayers, functions, arguments

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:math';
+// import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,47 +13,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text(
-            "Ask Me Anything",
-            style: TextStyle(color: Colors.white),
+          title: const Center(
+            child: Text(
+              "Xylophone",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-          backgroundColor: Colors.blue.shade900,
+          backgroundColor: Colors.grey,
         ),
-        body: MagicBall(),
+        body: const SafeArea(child: xylophone()),
       ),
     );
   }
 }
 
-class MagicBall extends StatefulWidget {
-  const MagicBall({super.key});
+class xylophone extends StatefulWidget {
+  const xylophone({super.key});
 
   @override
-  State<MagicBall> createState() => _MagicBallState();
+  State<xylophone> createState() => _xylophoneState();
 }
 
-class _MagicBallState extends State<MagicBall> {
-  int ball = 1;
-  void magicBallClick() {
-    setState(() {
-      ball = Random().nextInt(4) + 1;
-    });
+class _xylophoneState extends State<xylophone> {
+
+  void playSound(int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(DeviceFileSource('assets/note$soundNumber.wav'));
+  }
+
+  buildKey(Color color, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: TextButton(
-          onPressed: () {
-            magicBallClick();
-          },
-          child: Image.asset("images/ball$ball.png"),
-        ),
-      ),
+    return Column(
+      children: [
+        buildKey(Colors.red,1),
+        buildKey(Colors.orange,2),
+        buildKey(Colors.yellow,3),
+        buildKey(Colors.green,4),
+        buildKey(Colors.teal,5),
+        buildKey(Colors.blue,6),
+        buildKey(Colors.purple,7),
+      ],
     );
   }
 }
