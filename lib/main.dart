0bf3da11282893083e8f4fe0503dca0788fa9081
Sumit <sumokhan76342@gmail.com>
2024-08-26 20:31:44 +0530

    added qustion answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,175 @@
-// import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const Quizzler());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              "Xylophone",
-              style: TextStyle(color: Colors.white),
-            ),
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
-          backgroundColor: Colors.grey,
         ),
-        body: const SafeArea(child: xylophone()),
       ),
     );
   }
 }
 
-class xylophone extends StatefulWidget {
-  const xylophone({super.key});
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
 
   @override
-  State<xylophone> createState() => _xylophoneState();
+  _QuizPageState createState() => _QuizPageState();
 }
 
-class _xylophoneState extends State<xylophone> {
+class _QuizPageState extends State<QuizPage> {
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
 
-  void playSound(int soundNumber) async {
-    final player = AudioPlayer();
-    await player.play(DeviceFileSource('assets/note$soundNumber.wav'));
-  }
+  List<bool> answerKey = [false, true, true];
 
-  buildKey(Color color, int soundNumber) {
-    return Expanded(
-      child: TextButton(
-        onPressed: () {
-          playSound(soundNumber);
-        },
-        child: Container(
-          color: color,
-        ),
-      ),
-    );
-  }
+  int questionNumber = 0;
+
+  List<Icon> scoreKeeper = [];
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        buildKey(Colors.red,1),
-        buildKey(Colors.orange,2),
-        buildKey(Colors.yellow,3),
-        buildKey(Colors.green,4),
-        buildKey(Colors.teal,5),
-        buildKey(Colors.blue,6),
-        buildKey(Colors.purple,7),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  // checking correct answer is true
+                  if (answerKey[questionNumber] == true) {
+                    print("Right clicked ✅✅");
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  } else {
+                    print("Wrong clicked ❌❌");
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
+
+                  if (questionNumber == 2) {
+                    print("yes it's 2");
+                    questionNumber = 0;
+                  } else {
+                    questionNumber += 1;
+                    print(questionNumber);
+                  }
+                });
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              // color: Colors.red,
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                // checking correct answer is false
+                setState(() {
+                  if (answerKey[questionNumber] == false) {
+                    print("Right clicked ✅✅");
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  } else {
+                    print("Wrong clicked ❌❌");
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
+                  if (questionNumber == 2) {
+                    questionNumber = 0;
+                  } else {
+                    questionNumber += 1;
+                    print(questionNumber);
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
